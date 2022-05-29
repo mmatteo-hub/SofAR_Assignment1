5fb81860fd87794cde19ab77e91778b8f44812f0
mmatteo-hub <S4636216@studenti.unige.it>
2022-05-29 12:23:12 +0200

    still to fix dependencies

---
+++ src/assignment_pkg/launch/assignment_launch.py
@@ -0,0 +1,41 @@
+import os
+import pathlib
+import launch
+from launch_ros.actions import Node
+from launch import LaunchDescription
+from ament_index_python.packages import get_package_share_directory
+from webots_ros2_driver.webots_launcher import WebotsLauncher
+
+def generate_launch_description():
+    package_dir = get_package_share_directory('assignment_pkg')
+    robot_description = pathlib.Path(os.path.join(package_dir, 'resource', 'tiago_robot.urdf')).read_text()
+
+    webots = WebotsLauncher(
+        world=os.path.join(package_dir, 'worlds', 'factory.wbt')
+    )
+
+    robot = Node(
+        package='webots_ros2_driver',
+        executable='driver',
+        output='screen',
+        parameters=[
+            {'robot_description': robot_description},
+        ]
+    )
+
+    #controller = Node(
+    #    package='assignment_pkg',
+    #    executable='controller',
+    #)
+
+    return LaunchDescription([
+        webots,
+        robot,
+        controller,
+        launch.actions.RegisterEventHandler(
+            event_handler=launch.event_handlers.OnProcessExit(
+                target_action=webots,
+                on_exit=[launch.actions.EmitEvent(event=launch.events.Shutdown())],
+            )
+        )
+    ])
---
+++ src/assignment_pkg/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+package_name = 'assignment_pkg'
+
+data_files=[]
+data_files.append(('share/ament_index/resource_index/packages', ['resource/' + package_name]))
+data_files.append(('share/' + package_name + '/launch', ['launch/assignment_launch.py']))
+data_files.append(('share/' + package_name + '/worlds', ['worlds/factory.wbt']))
+data_files.append(('share/' + package_name + '/resource', ['resource/tiago_robot.urdf']))
+data_files.append(('share/' + package_name, ['package.xml']))
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=data_files,
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='matteomaragliano',
+    maintainer_email='S4636216@studenti.unige.it',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            #'controller = assignment_pkg.controller:main',
+        ],
+    },
+)
