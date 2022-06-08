00ca01cfb7c5ecb953b0d6b31a4efee03b33cb05
mmatteo-hub <S4636216@studenti.unige.it>
2022-06-08 10:25:49 +0200

    gitignore working

--- build/sofar_assignment_pkg/build/lib/sofar_assignment_pkg/controller_tiago.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print('Hi from sofar_assignment_pkg.')
-
-
-if __name__ == '__main__':
-    main()
--- build/webots_ros2_control/colcon_command_prefix_build.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/matteomaragliano/Desktop/SofAR_Assignment1/install/webots_ros2_driver/share/webots_ros2_driver/package.sh"
