ff401891936dba0e7dfff055bb04e1ea0ec39c9c
mmatteo-hub <S4636216@studenti.unige.it>
2022-06-30 16:36:56 +0200

    added modification to the UI. Still to fix the module errors

--- src/robot_ui/robot_ui/robot_ui.py
+++ src/robot_ui/robot_ui/robot_ui.py
@@ -1,5 +1,6 @@
 import rclpy
-from nav2_simple_commander.robot_navigator import BasicNavigator, TaskResult
+from rclpy import Node
+from nav2_simple_commander.robot_navigator import BasicNavigator, TaskResult, NavigationResult
 from rclpy.duration import Duration
 from geometry_msgs.msg import PoseStamped
 
@@ -11,8 +12,8 @@ class RobotUI(Node):
         self._navigator_r1 = BasicNavigator()
         self._navigator_r2 = BasicNavigator()
 
-        self._pub_r1_ = self.create_publisher(PoseStamped, '/robot1/goal_to_pose', 1)
-        self._pub_r2_ = self.create_publisher(PoseStamped, '/robot2/goal_to_pose', 1)
+        self._navigator_r1.waitUntilNav2Active()
+        self._navigator_r2.waitUntilNav2Active()
 
     def _set_initial_pose(self):
         inital_pose_r1 = PoseStamped()
@@ -27,18 +28,21 @@ class RobotUI(Node):
         inital_pose_r1.pose.position.x = -5.0
         inital_pose_r1.pose.position.y = 1.0
         inital_pose_r1.pose.position.z = 0.1
+        inital_pose_r1.pose.orientation.x = 0.0
+        inital_pose_r1.pose.orientation.y = 0.0
         inital_pose_r1.pose.orientation.z = 0.0
+        inital_pose_r1.pose.orientation.w = 1.0
 
         inital_pose_r2.pose.position.x = 5.0
         inital_pose_r2.pose.position.y = -1.0
         inital_pose_r2.pose.position.z = 0.1
-        inital_pose_r2.pose.orientation.z = 3.14
+        inital_pose_r1.pose.orientation.x = 0.0
+        inital_pose_r2.pose.orientation.y = 0.0
+        inital_pose_r2.pose.orientation.z = 0.0
+        inital_pose_r2.pose.orientation.w = 1.0
 
         self._navigator_r1.setInitialPose(inital_pose_r1)
         self._navigator_r2.setInitialPose(inital_pose_r2)
-
-        self._navigator_r1.waitUntilNav2Active()
-        self._navigator_r2.waitUntilNav2Active()
 
     def _prompt_commands(self):
         self.get_logger().info("Choose one of the possible commands:")
@@ -58,14 +62,56 @@ class RobotUI(Node):
         self.get_logger().info("Prompt the orientation:")
         orientation = float(input())
 
-        self._pose = [posx, posy, orientation]
+        self._pose = [posx, posy, 0, 0, 0, 0, 1]
 
         if self._namespace == "/robot1":
-            self._pub_r1_.publish(self._pose)
+            self._navigator_r1.goToPose(self._pose)
+
+            i = 0
+
+            while not self._navigator_r1.isNavComplete():
+                i = i + 1
+                feedback = self._navigator_r1.getFeedback()
+                if feedback and i % 5 == 0:
+                    print('Distance remaining: ' + '{:.2f}'.format(feedback.distance_remaining) + ' m')
+
+                    if(Duration.from_msg(feedback.navigation_time) > Duration(seconds=600.0)):
+                        self._navigator_r1.cancelNav()
+            
+            result = self._navigator_r1.getResult()
+            if result == NavigationResult.SUCCEEDED:
+                print('Goal succeeded')
+            elif result == NavigationResult.CANCELED:
+                print('Goal canceled')
+            elif result == NavigationResult.FAILED:
+                print('Goal failed')
+            else:
+                print('Invalid return status')
+
         elif self._namespace == "/robot2":
-            self._pub_r2_.publish(self._pose)
-        
-        
+            self._navigator_r2.goToPose(self._pose)
+
+            i = 0
+
+            while not self._navigator_r2.isNavComplete():
+                i = i + 1
+                feedback = self._navigator_r2.getFeedback()
+                if feedback and i % 5 == 0:
+                    print('Distance remaining: ' + '{:.2f}'.format(feedback.distance_remaining) + ' m')
+
+                    if(Duration.from_msg(feedback.navigation_time) > Duration(seconds=600.0)):
+                        self._navigator_r2.cancelNav()
+            
+            result = self._navigator_r2.getResult()
+            if result == NavigationResult.SUCCEEDED:
+                print('Goal succeeded')
+            elif result == NavigationResult.CANCELED:
+                print('Goal canceled')
+            elif result == NavigationResult.FAILED:
+                print('Goal failed')
+            else:
+                print('Invalid return status')
+
 def main(args=None):
     # Initializing the Ros2 library
     rclpy.init(args=args)
