9ca05d9dfdf8809fc58892dbec9b3947307ae66e
mmatteo-hub <S4636216@studenti.unige.it>
2022-07-02 11:56:30 +0200

    policy controller

--- src/policy_controller/policy_controller/policy_controller.py
+++ src/policy_controller/policy_controller/policy_controller.py
@@ -1,9 +1,12 @@
 import rclpy
 from rclpy.node import Node
+from rclpy.action import ActionClient
 
 from std_msgs.msg import String
 from nav_msgs.msg import Path
 from nav_msgs.msg import Odometry
+from nav2_msgs.action import Wait
+from geometry_msgs.msg import PoseStamped
 from action_msgs.srv import CancelGoal
 
 class PolicyController(Node):
@@ -18,8 +21,20 @@ class PolicyController(Node):
         # over a topic named 'topic' with the specified callback.
         self.sub_r1 = self.create_subscription(Path, 'robot1/received_global_plan', self.on_robot1_path, 10)
         self.sub_r2 = self.create_subscription(Path, 'robot2/received_global_plan', self.on_robot2_path, 10)
+        # Cretting a publisher for each robot to store the goal pose
+        self.sub_r1_goal = self.create_subscription(PoseStamped, '/robot1/goal_pose', self.robot1_goal, 10)
+        self.sub_r2_goal = self.create_subscription(PoseStamped, '/robot2/goal_pose', self.robot2_goal, 10)
         # Odom subscribers
         self.sub_odom1 = self.create_subscription(Odometry, 'robot1/odom', self.on_robot1_odom, 10)
+        self.sub_odom2 = self.create_subscription(Odometry, 'robot2/odom', self.on_robot2_odom, 10)
+        # Wait client
+        self._action_client = ActionClient(self, Wait, '/robo1/wait')
+
+    def robot1_goal(self, msg):
+        self._goal1_pose = msg.pose
+
+    def robot2_goal(self, msg):
+        self._goal2_pose = msg.pose
         
 
     def on_robot1_path(self, msg):
@@ -27,8 +42,8 @@ class PolicyController(Node):
         # Therefore, the subscriber is not usefull anymore
         self.destroy_subscription(self.sub_r1)
         # Obtaining the poses
-        self._poses_1 = GPListener.parse_poses_from_msg(msg)
-        print("Path from robot1 received\n");
+        self._poses_1 = PolicyController.parse_poses_from_msg(msg)
+        print("Path from robot1 received\n")
         # Checks if the intersection can be computed and computes it
         self.check_and_intersect()
         
@@ -38,8 +53,8 @@ class PolicyController(Node):
         # Therefore, the subscriber is not usefull anymore
         self.destroy_subscription(self.sub_r2)
         # Obtaining the poses
-        self._poses_2 = GPListener.parse_poses_from_msg(msg)
-        print("Path from robot2 received\n");
+        self._poses_2 = PolicyController.parse_poses_from_msg(msg)
+        print("Path from robot2 received\n")
         # Checks if the intersection can be computed and computes it
         self.check_and_intersect()
         
@@ -56,7 +71,7 @@ class PolicyController(Node):
         # Check if both the paths are received
         if self._poses_1 == None or self._poses_2 == None :
             return
-        print("Both path received\n");
+        print("Both path received\n")
         
         # Computing the intersections of the two paths
         intersections = []
@@ -92,11 +107,23 @@ class PolicyController(Node):
         if self._next_intersection_pose == None :
             return
             
-        current_pos = msg.pose.pose.position
+        self._check_and_wait_or_go(msg.pose.pose.position)
+
+    def on_robot2_odom(self, msg):
+        self._last_pose2 = msg.pose
+    
+    def _on_wait_finish(self, future):
+        self._check_and_wait_or_go(self.last_pose1)
+
+    def _check_and_wait_or_go(self, current_pos):
         if is_same_pose_threshold(current_pos, self._poses_1[self._next_intersection_pose], 0.16) :
             print("Intersection pose reached!")
-            follow_path_cancel_service = self.create_client(CancelGoal, '/robot1/navigate_to_pose/_action/cancel_goal')
-            follow_path_cancel_service.call_async(CancelGoal.Request())
+            self.last_pose1 = current_pos
+            if is_same_pose_threshold(self.last_pose1, self._last_pose2, 0.25):
+                _goal = Wait.Goal()
+                _goal.sec = 3
+                future = self._action_client.send_goal_async(_goal)
+                future.add_done_callback(self._on_wait_finish)
 
 # Return true if line segments AB and CD intersect
 def intersect(a,b,c,d):
--- src/policy_controller/setup.py
+++ src/policy_controller/setup.py
@@ -1,26 +1,29 @@
 from setuptools import setup
 
 package_name = 'policy_controller'
+_maintainer = ['matteomaragliano', 'ilmusu', 'danipari']
+_maintainer_email = ['4636216@studenti.unige.it','4670261@studenti.unige.it','4670964@studenti.unige.it']
+
+data_files = []
+data_files.append(('share/ament_index/resource_index/packages', ['resource/' + package_name]))
+data_files.append(('share/' + package_name + '/launch', ['launch/policy_launch.py']))
+data_files.append(('share/' + package_name, ['package.xml']))
 
 setup(
     name=package_name,
     version='0.0.0',
     packages=[package_name],
-    data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-    ],
+    data_files=data_files,
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='ilmusu',
-    maintainer_email='mattia.musumeci@yahoo.it',
+    maintainer=_maintainer,
+    maintainer_email=_maintainer_email,
     description='TODO: Package description',
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'policy_controller = policy_controller.policy_controller:main'
+            'policy_controller = policy_controller.policy_controller:main',
         ],
     },
 )
