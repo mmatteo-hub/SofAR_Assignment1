31e2f2246f972972151898a9b996b39b61517eb1
Mattia Musumeci <mattia.musumeci@yahoo.it>
2022-07-03 16:25:03 +0200

    rewrote the policy controller

--- src/policy_controller/policy_controller/policy_controller.py
+++ src/policy_controller/policy_controller/policy_controller.py
@@ -2,10 +2,11 @@ import rclpy
 from rclpy.node import Node
 from rclpy.action import ActionClient
 
+from rcl_interfaces.msg import ParameterDescriptor
+from rcl_interfaces.msg import ParameterType
 from std_msgs.msg import String
 from nav_msgs.msg import Path
 from nav_msgs.msg import Odometry
-from nav2_msgs.action import Wait
 from geometry_msgs.msg import PoseStamped
 from action_msgs.srv import CancelGoal
 
@@ -13,147 +14,158 @@ class PolicyController(Node):
 
     def __init__(self):
         super().__init__('policy_controller')
+        # Declaring some parameters to personalize the node
+        self.declare_parameter('robot_names', descriptor=ParameterDescriptor(type=ParameterType.PARAMETER_STRING_ARRAY))
+        self.declare_parameter('path_distance_threshold', 1.0)
+        self.declare_parameter('robot_intersection_threshold', 1.0)
+        self.declare_parameter('robots_distance_threshold', 1.5) 
+        # Obtaining the parameters
+        # These are the robots names ordered by priority
+        self._robots = self.get_parameter('robot_names').get_parameter_value().string_array_value
+        # This is the threshold for the path distance to compute the intersections
+        pthreshold = self.get_parameter('path_distance_threshold').get_parameter_value().double_value
+        self._pthreshold2 = pthreshold*pthreshold
+        # This is the threshold for the distance between the robot and the intersection
+        ithreshold = self.get_parameter('robot_intersection_threshold').get_parameter_value().double_value
+        self._ithreshold2 = ithreshold*ithreshold
+        # This is the threshold for the relative distance between the two robots
+        rthreshold = self.get_parameter('robots_distance_threshold').get_parameter_value().double_value
+        self._rthreshold2 = rthreshold*rthreshold
+        
+        # There must be at least two robots specified
+        if not self._robots or len(self._robots) != 2 :
+            self.get_logger().error("Not enogh specified robots")
+            return
         
         # Initializing variables
-        self._poses_1 = None
-        self._poses_2 = None
-        self._next_intersection_pose = None
-        self._is_robot1_waiting = False
+        # This contains the last recorded positions of the two robots.
+        self._poses = [None, None]
+        # This contains the paths of the two robots.
+        self._paths = [[], []]
+        # The next intersection positon
+        self._intersection_pose = None
+        # If the robot with minor priority is waiting
+        self._robot_waiting = False
         
-        # Receiving the global plan
-        self.sub_r1 = self.create_subscription(Path, 'robot1/received_global_plan', self.on_robot1_path, 10)
-        self.sub_r2 = self.create_subscription(Path, 'robot2/received_global_plan', self.on_robot2_path, 10)
-        # Receiving the position of the robot
-        self.sub_odom1 = self.create_subscription(Odometry, 'robot1/odom', self.on_robot1_odom, 10)
-        self.sub_odom2 = self.create_subscription(Odometry, 'robot2/odom', self.on_robot2_odom, 10)
-        # Wait client
-        self._action_client = ActionClient(self, Wait, '/robo1/wait')
+        # These are the topics where the plans are published when computed.
+        # We need to subscribe to these topics to compute the intersections between the plans.
+        self.sub_path0 = self.create_subscription(Path, self._robots[0]+'/received_global_plan', lambda msg: self.on_new_path_received(0, msg), 10)
+        self.sub_path1 = self.create_subscription(Path, self._robots[1]+'/received_global_plan', lambda msg: self.on_new_path_received(1, msg), 10)
+        # Once we have computed the intersections, we need to know where the two robots are.
+        self.create_subscription(Odometry, self._robots[0]+'/odom', lambda msg: self.on_robot_odom(0, msg), 10)
+        self.create_subscription(Odometry, self._robots[1]+'/odom', lambda msg: self.on_robot_odom(1, msg), 10)
         
 
-    def on_robot1_path(self, msg):
-        # The global path is published only once
-        # Therefore, the subscriber is not usefull anymore
-        self.destroy_subscription(self.sub_r1)
-        # Obtaining the poses
-        self._poses_1 = PolicyController.parse_poses_from_msg(msg)
-        print("Path from robot1 received")
-        # Checks if the intersection can be computed and computes it
-        self.check_and_intersect()
-        
+    def on_new_path_received(self, robot_id, msg):
+        if robot_id == 0:
+            self.destroy_subscription(self.sub_path0)
+        else :
+            self.destroy_subscription(self.sub_path1)
             
-    def on_robot2_path(self, msg):
-        # The global path is published only once
-        # Therefore, the subscriber is not usefull anymore
-        self.destroy_subscription(self.sub_r2)
-        # Obtaining the poses
-        self._poses_2 = PolicyController.parse_poses_from_msg(msg)
-        print("Path from robot2 received")
-        # Checks if the intersection can be computed and computes it
-        self.check_and_intersect()
-        
-        
-    def parse_poses_from_msg(msg):
+        # Creating a new empty list
         poses = []
-        # Obtaining only the actual poses
+        # Appending only the positions
         for pose_stamped in msg.poses :
             poses.append(pose_stamped.pose.position)
-        return poses
+        # Storing the positions list
+        self._paths[robot_id] = poses
+        
+        # A new path is received, so the old data is not valid anymore
+        self._intersection_pose = None
+        # TODO if the robot stopped, it should start again
+        
+        self.get_logger().info("Received path for "+self._robots[robot_id]+".")
+        
+        # Checks if the intersection can be computed and computes it
+        self.check_and_intersect()
         
         
     def check_and_intersect(self):
         # Check if both the paths are received
-        if self._poses_1 == None or self._poses_2 == None :
+        if not self._paths[0] or not self._paths[1] :
             return
-        print("Both path received")
         
-        # Computing the intersections of the two paths
-        intersections = []
         # Iterating for each segment in the two paths
-        for id1 in range(0, len(self._poses_1)-1) :
-            for id2 in range(0, len(self._poses_2)-1) :
-                # A segment in path 1
-                s1p1 = self._poses_1[id1]
-                s1p2 = self._poses_1[id1+1]
-                # A segment in path 2
-                s2p1 = self._poses_2[id2]
-                s2p2 = self._poses_2[id2+1]
+        # NB. It is foundamentally important that the outer cycle is the one
+        # related to the robot with the minor priority
+        for i1 in range(len(self._paths[1])) :
+            for i0 in range(len(self._paths[0])) :
+                # Two points from the the two paths
+                p0 = self._paths[0][i0]
+                p1 = self._paths[1][i1]
                 # Checking for intersection
-                if intersect(s1p1, s1p2, s2p1, s2p2) :
-                    intersections.append([id1, id2])
-                  
-        # Logging intersections  
-        print("Found " + str(len(intersections)) + " intersections.")
-        for inters in intersections :
-            i1 = inters[0]
-            i2 = inters[1]
-            print("First segment:")
-            print(str(self._poses_1[i1]) + "\n" + str(self._poses_1[i1+1]))
-            print("Second segment:")
-            print(str(self._poses_2[i2]) + "\n" + str(self._poses_2[i2+1]))
-            
-        # Storing the next intersection
-        if len(intersections) > 0 :
-            self._next_intersection_pose = intersections[0][0]
-            
-            
-    def on_robot1_odom(self, msg):
-        # Storing the last received position of the robot 1
-        self._last_pose1 = msg.pose.pose.position
+                if dist2(p0, p1) < self._pthreshold2 :
+                    self._intersection_pose = p1
+                    break
+            else:
+                continue
+            break
+                    
+        # Logging next intersection if found
+        if not self._intersection_pose == None :
+            pose_str = "( "+str(self._intersection_pose.x)+" , "+str(self._intersection_pose.y)+" )"
+            self.get_logger().info("Found intersection at: "+pose_str+" .")
+        else :
+            self.get_logger().info("Found no intersection")
         
-        # Check if there is a next intersection
-        if self._next_intersection_pose == None :
+        
+    def on_robot_odom(self, robot_id, msg):
+        # Storing the last received position of the corresponding robot
+        self._poses[robot_id] = msg.pose.pose.position
+        
+        # Check if the robot should wait
+        if robot_id == 1 and not self._intersection_pose == None :
+            self._maybe_stop_robot(robot_id)
+            return
+        
+        # Check if the robot should restart moving
+        if robot_id == 0 and self._robot_waiting == True :
+            self._maybe_restart_robot()
             return
             
-        self._check_and_wait_or_go()
 
-    def on_robot2_odom(self, msg):
-        # Storing the last received position of the robot 1
-        self._last_pose2 = msg.pose.pose.position
-
-    # NB. This always checks for robot1
-    def _check_and_wait_or_go(self):
+    def _maybe_stop_robot(self, robot_id):
         # Check if the robot is near enough to the intersection
-        if not self._is_robot1_waiting and is_same_pose_threshold(self._last_pose1, self._poses_1[self._next_intersection_pose], 0.16) :
-            print("Intersection pose reached!")
-            # Check if the two robots are near enough
-            if is_same_pose_threshold(self._last_pose1, self._last_pose2, 0.25):
-                print("Starting to wait...")
-                # Making the robot 1 wait
-                self._is_robot1_waiting = True
-                _goal = Wait.Goal()
-                _goal.time.sec = 3
-                future = self._action_client.send_goal_async(_goal)
-                future.add_done_callback(self._on_wait_finish)
+        # And then if the two robots are near enough
+        if dist2(self._poses[robot_id], self._intersection_pose) < self._ithreshold2 :
+            self.get_logger().info("Intersection pose reached")
+            if dist2(self._poses[0], self._poses[1]) < self._rthreshold2 :
+                self.get_logger().info("Starting to wait...")
+                # Canceling the goal of the robot with the specified id
+                stop_service = self.create_client(CancelGoal, self._robots[robot_id]+'/navigate_to_pose/_action/cancel_goal')
+                stop_service.call_async(CancelGoal.Request())
+                # Flag for making the robot restart
+                self._robot_waiting = True
+         
+           
+    def _maybe_restart_robot(self, robot_id):
+        # Check if the two robots are far enough
+        if dist2(self._poses[0], self._poses[1]) > self._rthreshold2 + 0.5 :
+            # TODO Send the previous goal to the old robot
+            self._robot_waiting = False
                 
-    def _on_wait_finish(self, future):
-        print("Finished waiting")
-        self._is_robot1_waiting = False
-        self._check_and_wait_or_go()
-
-# Return true if line segments AB and CD intersect
-def intersect(a,b,c,d):
-    return ccw(a,c,d) != ccw(b,c,d) and ccw(a,b,c) != ccw(a,b,d)
-
-# Checks if three points are in counter clockwise order
-def ccw(a,b,c):
-    return (c.y-a.y) * (b.x-a.x) > (b.y-a.y) * (c.x-a.x)
-
-def is_same_pose_threshold(pos1, pos2, th2):
-    f0 = pos1.x-pos2.x
-    f1 = pos1.y-pos2.y
-    dist2 = f0*f0+f1*f1
-    return dist2 < th2
+                
 
 def main(args=None):
+    # Initializing ROS with the parameters
     rclpy.init(args=args)
-
-    listener = PolicyController()
-    rclpy.spin(listener)
-
-    listener.destroy_node()
+    # Creating the PolicyController node
+    controller = PolicyController()
     
-    rclpy.shutdown()
+    # Spinning the controller until interrupted
+    try :
+        rclpy.spin(controller)
+    except KeyboardInterrupt:
+        # When interrupted
+        controller.destroy_node()
+        rclpy.shutdown()
+    
 
+def dist2(p0, p1) :
+    f0 = p0.x-p1.x
+    f1 = p0.y-p1.y
+    return f0*f0+f1*f1
+    
 
 if __name__ == '__main__':
     main()
