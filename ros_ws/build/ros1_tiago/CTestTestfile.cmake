88518b1fa755484d6d2bb96dbbe72e0c32672b25
mmatteo-hub <4636216@studenti.unige.it>
2022-07-21 15:46:11 +0200

    ws

--- ros_ws/build/ros1_tiago/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/teo/ros_ws/src/ros1_tiago
-# Build directory: /home/teo/ros_ws/build/ros1_tiago
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
